86f07bff50bfc1ff57c8a4ef3fcd7819b5b1b300
sirajsayyed06 <121347597+sirajsayyed06@users.noreply.github.com>
2025-11-07 23:41:59 +0530

    Update settings.py

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [ BASE_DIR / '/static/' ]
 
 # Default primary key field type
@@ -129,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = [ BASE_DIR / 'media' ]
+
 
 
 
